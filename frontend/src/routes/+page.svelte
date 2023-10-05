eff5776f9437ce0fbcbe837cde8fcaa6f9d936c0
nonorze-VOXX <appieosugood@gmail.com>
2023-10-06 02:16:11 +0800

    tail wind work

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -2,9 +2,3 @@
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
 <h1 class="text-3xl font-bold underline">Hello world!</h1>
-
-<style lang="postcss">
-	:global(html) {
-		background-color: theme(colors.yellow.500);
-	}
-</style>

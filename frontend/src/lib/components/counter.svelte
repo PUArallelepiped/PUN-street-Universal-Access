34b0ec5aaf9e1f30ef0d0d1122ecd5b699a1fd8f
nonorze-VOXX <appieosugood@gmail.com>
2023-10-07 04:26:40 +0800

    counter component work

---
+++ frontend/src/lib/components/counter.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	let count = 0;
+	export let allowNegative: boolean = false;
+	function add() {
+		count += 1;
+	}
+	function sub() {
+		if (!allowNegative && count == 0) {
+			return;
+		}
+		count -= 1;
+	}
+</script>
+
+<button on:click={add}>+</button>
+<input type="text" bind:value={count} />
+<button on:click={sub}>-</button>

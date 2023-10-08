9492b3da3ca131bbd3728a83f0330ed19aed9c47
nonorze-VOXX <appieosugood@gmail.com>
2023-10-08 17:09:34 +0800

    item page

---
+++ frontend/src/routes/shops/[shop]/[item]/+page.server.ts
@@ -0,0 +1,8 @@
+import type { PageServerLoad } from './$types';
+
+export const load: PageServerLoad = async ({ params }) => {
+    return {
+        shop:params.shop,
+        item:params.item
+    };
+};

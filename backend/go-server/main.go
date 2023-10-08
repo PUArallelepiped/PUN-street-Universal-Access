53b31fe08faaf5dc22b329af68079a77bf491f19
huhuhu0420 <ghostcheng.00@gmail.com>
2023-10-08 20:00:39 +0800

    can connect sql

--- backend/go-server/main.go
+++ backend/go-server/main.go
@@ -2,24 +2,62 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	_helloDelivery "main/hello/delivery"
 	_helloRepo "main/hello/repository"
 	_helloUsecase "main/hello/usecase"
 
+	_storeDelivery "main/store/delivery"
+	_storeRepo "main/store/repository"
+	_storeUsecase "main/store/usecase"
+
 	"github.com/gin-gonic/gin"
 	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+
+	_ "github.com/lib/pq"
 )
 
+func init() {
+	viper.SetConfigFile(".env")
+	viper.SetConfigType("dotenv")
+	if err := viper.ReadInConfig(); err != nil {
+		logrus.Fatal("Fatal error config file: %v\n", err)
+	}
+}
+
 func main() {
+	logrus.Info("HTTP server started!!!")
+
+	restfulHost := viper.GetString("RESTFUL_HOST")
+	restfulPort := viper.GetString("RESTFUL_PORT")
+	// dbHost := viper.GetString("DB_HOST")
+	dbDatabase := viper.GetString("DB_DATABASE")
+	dbUser := viper.GetString("DB_USER")
+	dbPassword := viper.GetString("DB_PASSWORD")
+
+	db, err := sql.Open(
+		"postgres",
+		fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable", dbUser, dbPassword, dbDatabase),
+	)
+
+	if err != nil {
+		logrus.Fatal(err)
+	}
+	if err = db.Ping(); err != nil {
+		logrus.Fatal(err)
+	}
+
 	r := gin.Default()
 
-	db := sql.DB{}
-
-	helloRepo := _helloRepo.NewPostgressqlHelloRepo(&db)
+	helloRepo := _helloRepo.NewPostgressqlHelloRepo(db)
+	storeRepo := _storeRepo.NewPostgressqlStoreRepo(db)
 
 	helloUsecase := _helloUsecase.NewHelloUsecase(helloRepo)
+	storeUsecase := _storeUsecase.NewStoreUsecase(storeRepo)
 
 	_helloDelivery.NewHelloHandler(r, helloUsecase)
+	_storeDelivery.NewStoreHandler(r, storeUsecase)
 
-	logrus.Fatal(r.Run())
+	logrus.Fatal(r.Run(restfulHost + ":" + restfulPort))
 }
--- backend/go-server/store/repository/postgresql.go
+++ backend/go-server/store/repository/postgresql.go
@@ -17,7 +17,7 @@ func NewPostgressqlStoreRepo(db *sql.DB) domain.StoreRepo {
 }
 
 func (p *postgresqlStoreRepo) GetByID(ctx context.Context, id string) (*domain.Store, error) {
-	row := p.db.QueryRow("SELECT id, name, address, email, phome FROM stores WHERE id = $1", id)
+	row := p.db.QueryRow("SELECT id, name, address, email, phone FROM stores WHERE id = $1", id)
 	s := &domain.Store{}
 	if err := row.Scan(&s.ID, &s.Name, &s.Address, &s.Email, &s.Phone); err != nil {
 		logrus.Error(err)

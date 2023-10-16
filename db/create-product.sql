5ad0a1498c7c09107ce322b5f5ca7e377055ca21
wj461 <wj4wj461@gmail.com>
2023-10-17 04:40:04 +0800

    fix store to stores

--- db/create-product.sql
+++ db/create-product.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS product (
     product_id SERIAL PRIMARY KEY,
-    storeId SERIAL references store(store_id),
+    storeId SERIAL references stores(store_id),
     name VARCHAR(255) NOT NULL,
     describe VARCHAR(255),
     category_id INT,

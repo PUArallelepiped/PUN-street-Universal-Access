7decdf81b8fc4171cdd28f1411427eb01252bfae
wj461 <wj4wj461@gmail.com>
2023-10-17 04:30:55 +0800

    add FK and rename and fix format

---
+++ db/create-product.sql
@@ -0,0 +1,10 @@
+CREATE TABLE product (
+    product_id SERIAL PRIMARY KEY,
+    storeId SERIAL references store(store_id),
+    name VARCHAR(255) NOT NULL,
+    describe VARCHAR(255),
+    category_id INT,
+    picture BYTEA,
+    price INT NOT NULL,
+    stock INT NOT NULL
+);

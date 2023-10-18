da00035d8978ee0134bbd8df825e3431a434e92d
nonorze-VOXX <appieosugood@gmail.com>
2023-10-18 16:01:11 +0800

    fix prettier on zsh

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,9 +3,9 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [sveltekit()],
-	server:{
-		proxy:{
-			'/api': 'http:127.0.0.1:5000'
+	server: {
+		proxy: {
+			'/api': 'http://127.0.0.1:5000'
 		}
 	}
 });

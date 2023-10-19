20265493fef8cb58a20ef543fe8abffaed875c74
wj461 <wj4wj461@gmail.com>
2023-10-19 10:48:46 +0800

    add diff for insert db sql

--- db/insert-product.sql
+++ db/insert-product.sql
@@ -1,4 +1,4 @@
 INSERT INTO products VALUES
-    ((default), (SELECT store_id FROM stores WHERE store_id=1), 'name', 'good food', 10, decode('DEADBEEF', 'hex'), 123,2),
-    ((default), (SELECT store_id FROM stores WHERE store_id=2), 'name', 'good food', 10, decode('DEADBEEF', 'hex'), 123,2),
-    ((default), (SELECT store_id FROM stores WHERE store_id=3), 'name', 'good food', 10, decode('DEADBEEF', 'hex'), 123,2);
+    ((default), (SELECT store_id FROM stores WHERE store_id=1), 'name_1', 'good food_1', 10, decode('DEADBEEF', 'hex'), 100,1),
+    ((default), (SELECT store_id FROM stores WHERE store_id=2), 'name_2', 'good food_2', 20, decode('DEADBEEF', 'hex'), 200,2),
+    ((default), (SELECT store_id FROM stores WHERE store_id=3), 'name_3', 'good food_3', 30, decode('DEADBEEF', 'hex'), 300,3);

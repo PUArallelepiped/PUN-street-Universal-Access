d97f95040104267b33b27565dd09ab2485d530c1
wj461 <wj4wj461@gmail.com>
2023-10-21 21:53:28 +0800

    add new data yo insert store

--- db/i10-insert-store.sql
+++ db/i10-insert-store.sql
@@ -1,3 +1,4 @@
-INSERT INTO stores (name, address, email, phone) VALUES (
-    'im pasta', 'pun street', 'pasta@pasta.com', '0912345678'
-)
+INSERT INTO stores (name, address, email, phone) VALUES 
+    ('im pasta', 'pun street', 'pasta@pasta.com', '0912345678'),
+    ('im pasta', 'pun street', 'pasta@pasta.com', '0912345678'),
+    ('im pasta', 'pun street', 'pasta@pasta.com', '0912345678')

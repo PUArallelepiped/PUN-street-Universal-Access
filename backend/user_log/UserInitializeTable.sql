7139c2d49aa7869a2c24968189105033a2bd90b4
orangeOAO <k98008@gmail.com>
2023-10-23 10:49:18 +0000

    user

--- backend/user_log/UserInitializeTable.sql
+++ backend/user_log/UserInitializeTable.sql
@@ -1,10 +1,10 @@
 CREATE TABLE UserData(
   ID serial PRIMARY KEY,
-  Name varchar[32] not null,
-  Password varchar[32] not null,
-  Email char[64] unique not null,
-  Address char[64] not null,
-  PhoneNumber varchar[16] not null,
-  Birthday timestamp,
+  Name varchar(32) not null,
+  Password varchar(32) not null,
+  Email varchar(64) unique not null,
+  Address char(64) not null,
+  PhoneNumber varchar(16) not null,
+  Birthday DATE,
   Authority int not null
 );
--- backend/user_log/user_log.go
+++ backend/user_log/user_log.go
@@ -1,103 +1,129 @@
 package main
 
 import (
-    "database/sql"
-    "strings"
-    "fmt"
-    _ "github.com/lib/pq"
-    "net/mail"
+	"database/sql"
+	"fmt"
+	"net/mail"
+	"regexp"
+	"strings"
+	"time"
+
+	_ "github.com/lib/pq"
 )
 
-func validMailAddress(address string) (string, bool) {
-    addr, err := mail.ParseAddress(address)
-    if err != nil {
-    return "", false
-    }
-    return addr.Address, true
+// 手機號碼格式
+func validPhoneNumber(phoneNumber string) bool {
+	regex := `^8869[0-9]{8}$|^09[0-9]{8}$`
+	match, _ := regexp.MatchString(regex, phoneNumber)
+	return match
+
 }
-//找重複email 有重複false
+
+// email格式
+func validMailAddress(address string) (string, bool) {
+	addr, err := mail.ParseAddress(address)
+	if err != nil {
+		return "", false
+	}
+	return addr.Address, true
+}
+
+// 找重複email 有重複false
 func searchMailAddress(db *sql.DB, address string) bool {
-    rows, err := db.Query("SELECT id, email FROM UserData")
-    if err != nil {
-        fmt.Println("Error running query:", err)
-        return false
-    }
-    defer rows.Close()
+	rows, err := db.Query("SELECT id, Email FROM UserData")
+	if err != nil {
+		fmt.Println("Error running query:", err)
+		return false
+	}
+	defer rows.Close()
 
-    for rows.Next() {
-        var id int
-        var email string
-        if err := rows.Scan(&id, &email); err != nil {
-            fmt.Println("Error scanning row:", err)
-            return false
-        }
-        if strings.EqualFold(email, address) {
-            fmt.Println("This Email has been registered")
-            return false
-        }
-    }
+	for rows.Next() {
+		var id int
+		var email string
+		if err := rows.Scan(&id, &email); err != nil {
+			fmt.Println("Error scanning row:", err)
+			return false
+		}
 
-    return true
+		if strings.EqualFold(address, email) {
+			fmt.Println("This Email has been registered")
+			return false
+		}
+	}
+
+	return true
 }
 
 func main() {
-    // 建立数据库连接
-    connStr := "user=postgres dbname=postgres sslmode=disable"
-    db, err := sql.Open("postgres", connStr)
-    if err != nil {
-        fmt.Println("Error opening database:", err)
-        return
-    }
-    defer db.Close()
+	// 建立数据库连接
+	connStr := "user=orange dbname=user_data sslmode=disable"
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		fmt.Println("Error opening database:", err)
+		return
+	}
+	defer db.Close()
+	type UserData struct {
+		Name, Account, Address, PhoneNumber, Password string
+		Authority                                     int
 
-	var _name string
-    var _account string
-	var _address string
-	var _birthday string
-	var _phoneNumber string
-    var _passwd string
-	var _authority int
-	_birthday = "2002/12/12"
-	_name = "orange"
-    _account = "k98006@gmail.com"
-	_address ="Taiwan"
-	_phoneNumber ="091232242"
-    _passwd = "orange"
-    _authority =0;
-    // db.QueryRow("SELECT COUNT(*) FROM users").Scan(&_usercount)
-   
-    if addr, ok := validMailAddress(_account); ok {
-        fmt.Printf("value: %-30s valid email: %-10t address: %s\n", _account, ok, addr)
-    } else {
-        fmt.Printf("value: %-30s valid email: %-10t\n", _account, ok)
-    }
-    //搜尋
-    _TFwrite:=searchMailAddress(db,_account);
-    if _TFwrite{
-        fmt.Println("Can be register :D");
-    }
-    // 开始事务
-    tx, err := db.Begin()
-    if err != nil {
-        fmt.Println("Error starting transaction:", err)
-        return
-    }
-    
-    // 执行插入操作
-    if _TFwrite {
-		_, err = tx.Exec("INSERT INTO UserData (Name, Password, Email, Address, PhoneNumber, Birthday, Authority) VALUES ($1, $2, $3, $4, $5, $6, $7)", _name, _passwd, _account, _address, _phoneNumber, _birthday, _authority)
+		Birthday time.Time
+	}
+	//stirng2Time
+	date, err := time.Parse("2006/01/02", "2002/12/17")
+	if err != nil {
+		fmt.Println("日期解析错误:", err)
+		return
+	}
+	data := UserData{
+		Name:        "XDD",
+		Account:     "k98006@gmail.com",
+		Address:     "Taiwan",
+		Birthday:    date,
+		PhoneNumber: "0912455678",
+		Password:    "orange",
+		Authority:   0,
+	}
 
-        if err != nil {
-            fmt.Println("Error inserting data:", err)
-            tx.Rollback()
-            return
-        }
-        // 提交事务
-        err = tx.Commit()
-        if err != nil {
-            fmt.Println("Error committing transaction:", err)
-            return
-        }
-    }
+	// db.QueryRow("SELECT COUNT(*) FROM users").Scan(&_usercount)
+	if validPhoneNumber(data.PhoneNumber) {
+		fmt.Println("Correct PhoneNumber")
+	} else {
+		fmt.Println("Wrong PhoneNumber")
+	}
+
+	if addr, ok := validMailAddress(data.Account); ok {
+		fmt.Printf("value: %-30s valid email: %-10t address: %s\n", data.Account, ok, addr)
+	} else {
+		fmt.Printf("value: %-30s valid email: %-10t\n", data.Account, ok)
+	}
+	//搜尋
+	_TFwrite := searchMailAddress(db, data.Account)
+	if _TFwrite {
+		fmt.Println("Can be register :D")
+	}
+	// 开始事务
+	tx, err := db.Begin()
+	if err != nil {
+		fmt.Println("Error starting transaction:", err)
+		return
+	}
+
+	// 执行插入操作
+	if _TFwrite {
+		_, err = tx.Exec("INSERT INTO UserData (Name, Password, Email, Address, PhoneNumber, Birthday, Authority) VALUES ($1, $2, $3, $4, $5, $6, $7)", data.Name, data.Password, data.Account, data.Address, data.PhoneNumber, data.Birthday, data.Authority)
+
+		if err != nil {
+			fmt.Println("Error inserting data:", err)
+			tx.Rollback()
+			return
+		}
+		// 提交事务
+		err = tx.Commit()
+		if err != nil {
+			fmt.Println("Error committing transaction:", err)
+			return
+		}
+	}
 
 }

93c06023c270ee3243294fa13f9d82a0a8ab6ab2
huhuhu0420 <ghostcheng.00@gmail.com>
2023-10-28 01:35:43 +0800

    add pic

--- db/i10-store.sql
+++ db/i10-store.sql
@@ -1,4 +1,4 @@
 INSERT INTO stores (name, rate, rate_count, address, picture, description, shipping_fee, status) VALUES 
-    ('im pasta', 4, 100, 'pun street', NULL, 'you are not pasta', 35, 1),
-    ('number five',3.5, 5, 'pun street', NULL, 'special meal', 15, 1),
-    ('mos burger',4, 123, 'NTUT', NULL, 'moooooooooooooos', 55, 1)
+    ('im pasta', 4, 100, 'pun street', 'https://imgur.com/3i3tyXJ', 'you are not pasta', 35, 1),
+    ('number five',3.5, 5, 'pun street', 'https://imgur.com/3i3tyXJ', 'special meal', 15, 1),
+    ('mos burger',4, 123, 'NTUT', 'https://imgur.com/3i3tyXJ', 'moooooooooooooos', 55, 1)

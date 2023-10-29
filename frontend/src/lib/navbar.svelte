ead3fe39a86608b15805a32dcc51a833bf8d47d4
nonorze-VOXX <appieosugood@gmail.com>
2023-10-29 23:06:04 +0800

    navbar remake

--- frontend/src/lib/navbar.svelte
+++ frontend/src/lib/navbar.svelte
@@ -1,14 +1,18 @@
 <script lang="ts">
 	// import{store} from "$app/stores"
 	const routes = [
-		{ path: '/', title: '🏠' },
-		{ path: '/food', title: '🍔' },
-		{ path: '/shoppingCart', title: '🛒' }
+		{ path: '/', title: 'My profile' },
+		{ path: '/food', title: 'ShopList' },
+		{ path: '/shoppingCart', title: 'Check Order' },
+		{ path: '/shoppingCart', title: 'History' }
 	];
 </script>
 
-<div class=" m-2 flex justify-center space-x-20 bg-sky-900 p-7 text-xl text-gray-50">
-	{#each routes as r}
-		<a href={r.path}>{r.title}</a>
-	{/each}
+<div class="flex h-20 place-content-between bg-white">
+	<div class="m-5 inline-block w-1/3 px-11 text-3xl">PUN street access</div>
+	<div class="flex h-full items-center justify-end gap-10 px-10">
+		{#each routes as r}
+			<a href={r.path} class="block p-2">{r.title}</a>
+		{/each}
+	</div>
 </div>

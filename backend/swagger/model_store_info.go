5250c1e113a961448c885f558057764d80d21ecd
huhuhu0420 <ghostcheng.00@gmail.com>
2023-10-29 20:01:47 +0800

    rm email in store

--- backend/swagger/model_store_info.go
+++ backend/swagger/model_store_info.go
@@ -9,7 +9,6 @@
 package swagger
 
 type StoreInfo struct {
-
 	StoreId int64 `json:"store_id"`
 
 	Description string `json:"description"`
@@ -17,8 +16,6 @@ type StoreInfo struct {
 	Name string `json:"name"`
 
 	Address string `json:"address"`
-
-	Email string `json:"email"`
 
 	Rate float32 `json:"rate"`
 

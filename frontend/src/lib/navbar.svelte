9ba9123428f07a4ccb650c917504675f14ca6f0e
nonorze-VOXX <appieosugood@gmail.com>
2023-10-30 09:24:39 +0800

    topleft string to link

--- frontend/src/lib/navbar.svelte
+++ frontend/src/lib/navbar.svelte
@@ -1,15 +1,15 @@
 <script lang="ts">
 	// import{store} from "$app/stores"
 	const routes = [
-		{ path: '/', title: 'My profile' },
-		{ path: '/food', title: 'ShopList' },
-		{ path: '/shoppingCart', title: 'Check Order' },
-		{ path: '/shoppingCart', title: 'History' }
+		{ path: '/', title: 'My profile' }, // TODO
+		{ path: '/shops', title: 'ShopList' },
+		{ path: '/', title: 'Check Order' }, // TODO
+		{ path: '/', title: 'History' } // TODO
 	];
 </script>
 
 <div class="flex h-20 place-content-between bg-white">
-	<div class="m-5 inline-block w-1/3 px-11 text-3xl">PUN street access</div>
+	<a href="/" class="flex h-full items-center justify-start px-11 text-3xl">PUN street access</a>
 	<div class="flex h-full items-center justify-end gap-10 px-10">
 		{#each routes as r}
 			<a href={r.path} class="block p-2">{r.title}</a>

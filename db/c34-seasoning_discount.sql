3cb00b100edc526e2af0832c07a4b64c7d038cfe
wj461 <wj4wj461@gmail.com>
2023-11-15 22:48:36 +0800

    fix db

--- db/c34-seasoning_discount.sql
+++ db/c34-seasoning_discount.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS seasoning_discount (
-    discount_id SERIAL REFERENCES discounts(discount_id),
+    discount_id SERIAL UNIQUE REFERENCES discounts(discount_id),
     discount_percentage INT NOT NULL,
     start_date DATE NOT NULL,
     end_date DATE NOT NULL
--- db/c35-shipping_discount.sql
+++ db/c35-shipping_discount.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS shipping_discount (
-    discount_id SERIAL REFERENCES discounts(discount_id),
+    discount_id SERIAL UNIQUE REFERENCES discounts(discount_id),
     max_price INT NOT NULL,
     store_id SERIAL REFERENCES stores(store_id)
 );

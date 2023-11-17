7361ba5563ecb6e5716cdd560951cff5c92f4d1b
wj461 <wj4wj461@gmail.com>
2023-11-17 21:37:38 +0800

    change order db

--- db/i40-cart.sql
+++ db/i40-cart.sql
@@ -1,5 +1,6 @@
 INSERT INTO carts (customer_id, cart_id, store_id, product_id, product_quantity, event_discount_id) VALUES
     (1, 1, 1, 2, 100, 1),
+    (1, 1, 1, 1, 100, 1),
     (1, 2, 1, 2, 33, 2),
     (3, 1, 2, 3, 100, 1),
     (1, 3, 1, 1, 7, 1)

da546d46f092b6218a4bb79f1c2d01d0e72880d2
wj461 <wj4wj461@gmail.com>
2023-11-17 21:26:20 +0800

    add disocunt insert

--- db/i40-cart.sql
+++ db/i40-cart.sql
@@ -1,4 +1,4 @@
-INSERT INTO carts (customer_id, product_id, store_id, cart_id, product_quantity, event_discount_id) VALUES
+INSERT INTO carts (customer_id, cart_id, store_id, product_id, product_quantity, event_discount_id) VALUES
     (1, 1, 1, 2, 100, 1),
     (1, 2, 1, 2, 33, 2),
     (3, 1, 2, 3, 100, 1),

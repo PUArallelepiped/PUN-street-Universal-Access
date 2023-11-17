8aa6d8ddd592be9f347625bcfda044bc525f7822
orangeOAO <k98008@gmail.com>
2023-11-17 16:07:32 +0000

    1117

--- backend/user/main.go
+++ backend/user/main.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"github.com/PUArallelepiped/PUN-street-Universal-Access/user/log"
+)
+
 func main() {
-	log.authenticateUser("apple", "orangeOAO123")
+
+	log.LogIn("orange", "orangeOAO123")
+
 }

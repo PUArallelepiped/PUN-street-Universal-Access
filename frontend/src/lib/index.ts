77af490d8b2cef6b52fe9d4157312ed784fb0445
nonorze-VOXX <appieosugood@gmail.com>
2023-11-22 02:00:55 +0800

    all thing look good

--- frontend/src/lib/index.ts
+++ frontend/src/lib/index.ts
@@ -7,3 +7,7 @@ import ProductCard from './components/PUA/productCard.svelte';
 export { ProductCard };
 import CartItemCard from './components/PUA/cartItemCard.svelte';
 export { CartItemCard };
+import Label from './components/PUA/Label.svelte';
+export { Label as PUALabel };
+import CartLabelBox from './components/PUA/cartLabelBox.svelte';
+export { CartLabelBox };
--- frontend/src/routes/cart/+page.svelte
+++ frontend/src/routes/cart/+page.svelte
@@ -1,8 +1,15 @@
 <script lang="ts">
-	import { CartItemCard } from '$lib/index';
+	import { CartItemCard, CartLabelBox } from '$lib/index';
+	let orderInfo = {
+		name: 'Order Name',
+		phone: 'Phone Number',
+		method: 'Taking Method',
+		totalShippingFee: 'Total Shipping Fee',
+		address: 'Taking Address'
+	};
 </script>
 
-<div class="flex justify-center">
+<div class="flex flex-col items-center justify-center gap-5">
 	<div class="flex max-w-7xl flex-col gap-3">
 		<div class="flex flex-col">
 			<div class="flex justify-between">
@@ -22,8 +29,11 @@
 		<div class="grid grid-cols-3 gap-3">
 			<CartItemCard></CartItemCard>
 			<CartItemCard></CartItemCard>
-			<CartItemCard></CartItemCard>
-			<CartItemCard></CartItemCard>
 		</div>
+		<div>
+			<div class="text-xl font-semibold leading-normal text-stone-600">Discount</div>
+			<hr class="border-orange-950" />
+		</div>
+		<CartLabelBox></CartLabelBox>
 	</div>
 </div>

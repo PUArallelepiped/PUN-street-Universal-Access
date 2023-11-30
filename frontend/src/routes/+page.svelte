b1e7e5db19b432bcf669ad6b0169cc5710a69e17
nonorze-VOXX <appieosugood@gmail.com>
2023-11-30 18:29:24 +0800

    update order page

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,11 +1,29 @@
 <script lang="ts">
 </script>
 
-<h1>Welcome to PUA</h1>
-<div class="bg-neutral-900">
-	<h1 class="text-3xl font-bold underline">Hello world!</h1>
-</div>
+<a href="/cart" class="m-4 rounded-lg border-2 border-blue-600 p-1 text-lg text-blue-600">cart</a>
+<a href="/history" class="m-4 rounded-lg border-2 border-blue-600 p-1 text-lg text-blue-600">
+	history
+</a>
+<a
+	href="/order/checkOrder"
+	class="m-4 rounded-lg border-2 border-blue-600 p-1 text-lg text-blue-600"
+>
+	checkOrder
+</a>
+<a
+	href="/order/updateOrderStatus"
+	class="m-4 rounded-lg border-2 border-blue-600 p-1 text-lg text-blue-600"
+>
+	updateOrderStatus
+</a>
+<a href="/shops" class="m-4 rounded-lg border-2 border-blue-600 p-1 text-lg text-blue-600">
+	shoplist
+</a>
 
-<p>here is root</p>
-
-<p>Visit <a href="./shops" class="text-blue-400"> Shop list</a></p>
+<a href="/shops/1" class="m-4 rounded-lg border-2 border-blue-600 p-1 text-lg text-blue-600">
+	shop
+</a>
+<a href="/shops/1/1" class="m-4 rounded-lg border-2 border-blue-600 p-1 text-lg text-blue-600">
+	item
+</a>

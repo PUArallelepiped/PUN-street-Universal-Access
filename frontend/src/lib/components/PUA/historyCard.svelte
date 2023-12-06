0b7bdad97e269e70714a7c3658f834df2dd977a5
nonorze-VOXX <appieosugood@gmail.com>
2023-12-06 21:40:19 +0800

    add star to history

--- frontend/src/lib/components/PUA/historyCard.svelte
+++ frontend/src/lib/components/PUA/historyCard.svelte
@@ -1,5 +1,15 @@
 <script>
 	import OkButton from './OkButton.svelte';
+	import Star from '$lib/assets/Star.svg';
+	import noStar from '$lib/assets/noStar.svg';
+	let rate = 4;
+	function getRateList() {
+		let rateList = [];
+		for (let i = 0; i < 5; i++) {
+			rateList.push(i < rate);
+		}
+		return rateList;
+	}
 </script>
 
 <div class="flex gap-5 rounded-xl bg-white p-2">
@@ -14,7 +24,17 @@
 		<div class=" text-2xl font-bold leading-relaxed text-PUA-dark-orange">Shop name</div>
 		<div class=" text-base font-bold leading-relaxed text-PUA-dark-orange">2020-10-11</div>
 		<div class=" text-base font-bold leading-relaxed text-PUA-dark-orange">NT$999</div>
-		<div class="font-semibold leading-normal text-PUA-dark-red">Rate store</div>
+
+		<div class="flex gap-2">
+			<div class="text-2xl font-semibold leading-normal text-PUA-dark-red">Rate store</div>
+			{#each getRateList() as r}
+				{#if r}
+					<img src={Star} alt="" />
+				{:else}
+					<img src={noStar} alt="" />
+				{/if}
+			{/each}
+		</div>
 	</div>
 	<div class="flex items-center justify-center">
 		<OkButton onclick={() => null} text="Detail"></OkButton>

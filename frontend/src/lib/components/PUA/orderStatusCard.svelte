dd7395cef9cff56995545cc002d943dc9470ef9a
OuChiaYun <j312079@gmail.com>
2023-12-12 14:32:56 +0800

    fix version

--- frontend/src/lib/components/PUA/orderStatusCard.svelte
+++ frontend/src/lib/components/PUA/orderStatusCard.svelte
@@ -13,22 +13,22 @@
 		<button class="flex items-center gap-5 rounded-xl bg-white p-4 shadow-xl">
 			<img src={statusCardContent.src} alt="" />
 			<div class="w-64 text-start font-bold">
-				<div class=" text-xl font-bold leading-relaxed text-PUA-dark-orange">
+				<div class=" text-PUA-dark-orange text-xl font-bold leading-relaxed">
 					{statusCardContent.time}
 				</div>
 				<div
-					class=" flex items-baseline gap-1 text-xl font-bold leading-relaxed text-PUA-dark-orange"
+					class=" text-PUA-dark-orange flex items-baseline gap-1 text-xl font-bold leading-relaxed"
 				>
 					<p class=" text-base">NT$</p>
 					<p>{statusCardContent.price}</p>
 				</div>
-				<div class=" text-xl font-bold leading-relaxed text-PUA-dark-orange">
+				<div class=" text-PUA-dark-orange text-xl font-bold leading-relaxed">
 					Order user : {statusCardContent.user}
 				</div>
 			</div>
 
 			<button
-				class=" flex h-20 w-40 items-center justify-center rounded-2xl bg-PUA-stone p-2 text-center font-bold leading-relaxed text-white hover:border-[3px] hover:border-PUA-stone hover:bg-white hover:text-PUA-stone"
+				class=" bg-PUA-stone hover:border-PUA-stone hover:text-PUA-stone flex h-20 w-40 items-center justify-center rounded-2xl p-2 text-center font-bold leading-relaxed text-white hover:border-[3px] hover:bg-white"
 			>
 				{statusCardContent.text}
 			</button>

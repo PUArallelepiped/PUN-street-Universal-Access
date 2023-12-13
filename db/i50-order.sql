b43d994c3c75ed5cd423b4d437c2b9f083232d12
wj461 <wj4wj461@gmail.com>
2023-12-13 23:05:45 +0800

    change orders db insert

--- db/i50-order.sql
+++ db/i50-order.sql
@@ -1,5 +1,5 @@
 INSERT INTO orders (user_id, cart_id, store_id, seasoning_discount_id, shipping_discount_id, status, total_price, Order_date, taking_address, taking_method) VALUES 
-    (1, 1, 1, 1, 1, 1, 100, '2020-01-01 00:00:00', '台北市', 1),
+    (1, 1, 1, 1, 1, 6, 100, '2020-01-01 00:00:00', '台北市', 1),
     (1, 2, 1, 2, 5, 1, 100, '2020-02-01 00:00:00', '台北市', 1),
     (3, 1, 2, 3, 6, 1, 100, '2020-03-01 00:00:00', '台北市', 1),
     (1, 3, 1, 4, 7, 1, 100, '2020-04-01 00:00:00', '台北市', 1)

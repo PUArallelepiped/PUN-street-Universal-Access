80bf55086d159b3a837d7eed51eb4d49d430d6bf
wj461 <wj4wj461@gmail.com>
2023-12-13 23:01:02 +0800

    do admin

---
+++ frontend/src/routes/admin/+page.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	import order_icon from '$lib/assets/order_icon.svg';
+
+</script>
+
+<div class="flex justify-center">
+    <div class="flex w-3/5 flex-col">
+        <div class="flex bg-PUA-stone">
+            <img src={order_icon} alt="" class="h-16 w-16 flex my-6 ml-10" />
+            <div class="flex flex-col m-7 justify-between">
+                <div>
+                    <p class="">11111</p>
+                    <p>11111</p>
+                </div>
+                <p>11111</p>
+            </div>
+        </div>
+        <div class="flex h-10">
+            <button class="w-full bg-slate-400">aa</button>
+            <button class="w-full bg-slate-100">aa</button>
+        </div>
+        <div class="h-screen bg-white">
+            <div class="flex bg-slate-300 my-3 mx-12 justify-between items-center">
+                <div class="flex bg-slate-300 justify-between items-center">
+                    <img src={order_icon} alt="" class="h-16 w-16 flex my-4 ml-6" />
+                    <div class="flex flex-col m-4">
+                        <p>11111</p>
+                        <p>11111</p>
+                    </div>
+                </div>
+                <div class="m-6">
+                    <button class="border-2 border-PUA-stone px-10 py-0">aaa</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>

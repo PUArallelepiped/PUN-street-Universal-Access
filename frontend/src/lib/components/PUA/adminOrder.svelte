ffaeab98fb57618087256f2d4eada2a0a3f02710
wj461 <wj4wj461@gmail.com>
2023-12-15 13:29:10 +0800

    orderCart work

---
+++ frontend/src/lib/components/PUA/adminOrder.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import order_icon from '$lib/assets/order_icon.svg';
+
+    export let firstCol: string;
+    export let secondCol: string;
+    export let type: number;
+</script>
+
+<div class="flex my-3 mx-12 justify-between border-2 border-PUA-dark-gray rounded-2xl">
+    <button class="w-full hover:bg-gray-200 rounded-xl">
+        <div class="flex justify-between items-center">
+            <div class="flex items-center">
+                {#if type == 0}
+                    <img src={order_icon} alt="" class="h-16 w-16 flex my-4 ml-6" />
+                {:else if type == 1}
+                    <img src={order_icon} alt="" class="h-16 w-16 flex my-4 ml-6" />
+                {:else}
+                    <img src={order_icon} alt="" class="h-16 w-16 flex my-4 ml-6" />
+                {/if}
+                <div class="flex text-left m-4 text-PUA-dark-red">
+                    {firstCol}<br>
+                    Order user : {secondCol}
+                </div>
+            </div>
+            <div class="m-6">
+                <button class="border-2 border-PUA-dark-red px-7 py-0 text-PUA-dark-red hover:bg-PUA-dark-red hover:text-white">Detail</button>
+            </div>
+        </div>
+    </button>
+</div>
--- frontend/src/routes/admin/+page.svelte
+++ frontend/src/routes/admin/+page.svelte
@@ -1,18 +1,58 @@
 <script lang="ts">
-	import order_icon from '$lib/assets/order_icon.svg';
+	import admin_icon from '$lib/assets/admin_icon.svg';
+    import AdminOrderCart from '$lib/components/PUA/adminOrder.svelte'
 
+    let orders: {
+        date: string,
+        user: string
+    }[] = [
+        {
+            date: "2020-10-11 19:20:50",
+            user: "user01"
+        },
+        {
+            date: "2020-10-11 19:20:50",
+            user: "user02"
+        },
+        {
+            date: "2020-10-11 19:20:50",
+            user: "user03"
+        },
+        {
+            date: "2020-10-11 19:20:50",
+            user: "user04"
+        }
+    ]
+
+    let users: {
+        name: string,
+        email: string
+    }[] = [
+        {
+            name: "sakana",
+            email: "r1111111"
+        },
+        {
+            name: "sakana dewanai",
+            email: "t1111111"
+        },
+        {
+            name: "sakana desu",
+            email: "fsddsdss"
+        },
+    ]
 </script>
 
-<div class="flex justify-center">
+<div class="flex justify-center font-bold">
     <div class="flex w-3/5 flex-col">
         <div class="flex bg-PUA-stone">
-            <img src={order_icon} alt="" class="h-16 w-16 flex my-6 ml-10" />
+            <img src={admin_icon} alt="" class="h-28 w-28 flex my-6 ml-10" />
             <div class="flex flex-col m-7 justify-between">
                 <div>
-                    <p class="">11111</p>
-                    <p>11111</p>
+                    <div class="text-PUA-gray text-2xl">sakana san</div>
+                    <div class="text-gray-300">t111105911</div>
                 </div>
-                <p>11111</p>
+                <div class="text-PUA-gray text-xl">address</div>
             </div>
         </div>
         <div class="flex h-10">
@@ -20,18 +60,14 @@
             <button class="w-full bg-slate-100">aa</button>
         </div>
         <div class="h-screen bg-white">
-            <div class="flex bg-slate-300 my-3 mx-12 justify-between items-center">
-                <div class="flex bg-slate-300 justify-between items-center">
-                    <img src={order_icon} alt="" class="h-16 w-16 flex my-4 ml-6" />
-                    <div class="flex flex-col m-4">
-                        <p>11111</p>
-                        <p>11111</p>
-                    </div>
-                </div>
-                <div class="m-6">
-                    <button class="border-2 border-PUA-stone px-10 py-0">aaa</button>
-                </div>
-            </div>
+            {#each orders as order}
+                <AdminOrderCart firstCol= {order.date} secondCol = {order.user}></AdminOrderCart>
+            {/each}
+        </div>
+        <div class="h-screen bg-white">
+            {#each users as user}
+                <AdminOrderCart firstCol= {user.name} secondCol = {user.email}></AdminOrderCart>
+            {/each}
         </div>
     </div>
 </div>

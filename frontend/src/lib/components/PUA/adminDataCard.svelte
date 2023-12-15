7c86834f6e4f0641cb1407c13721a85fb2ea23be
wj461 <wj4wj461@gmail.com>
2023-12-15 20:03:02 +0800

    format

--- frontend/src/lib/components/PUA/adminDataCard.svelte
+++ frontend/src/lib/components/PUA/adminDataCard.svelte
@@ -2,67 +2,75 @@
 	import order_icon from '$lib/assets/order_icon.svg';
 	import customer_icon from '$lib/assets/customer_icon.svg';
 	import seller_icon from '$lib/assets/seller_icon.svg';
-    import { onMount } from 'svelte';
- 
-    export let firstCol: string;
-    export let secondCol: string;
-    export let type: string;
+	import { onMount } from 'svelte';
 
-    export let ben: boolean = false;
-    let self: HTMLDivElement;
+	export let firstCol: string;
+	export let secondCol: string;
+	export let type: string;
 
+	export let ben: boolean = false;
+	let self: HTMLDivElement;
 
-    onMount(() => {
-        if (ben){
-            self.classList.remove("border-PUA-dark-gray");
-            self.classList.add("border-PUA-red");
-        }
-        else{
-            self.classList.add("border-PUA-dark-gray");
-            self.classList.remove("border-PUA-red");
-        }
-    });
+	onMount(() => {
+		if (ben) {
+			self.classList.remove('border-PUA-dark-gray');
+			self.classList.add('border-PUA-red');
+		} else {
+			self.classList.add('border-PUA-dark-gray');
+			self.classList.remove('border-PUA-red');
+		}
+	});
 
-    function changeButtonStatus() {
-        ben = !ben
-        if (ben){
-            self.classList.remove("border-PUA-dark-gray");
-            self.classList.add("border-PUA-red");
-        }
-        else{
-            self.classList.add("border-PUA-dark-gray");
-            self.classList.remove("border-PUA-red");
-        }
-    }
+	function changeButtonStatus() {
+		ben = !ben;
+		if (ben) {
+			self.classList.remove('border-PUA-dark-gray');
+			self.classList.add('border-PUA-red');
+		} else {
+			self.classList.add('border-PUA-dark-gray');
+			self.classList.remove('border-PUA-red');
+		}
+	}
 </script>
 
-<div bind:this={self} class="flex my-3 mx-12 justify-between border-2 border-PUA-dark-gray rounded-2xl">
-    <button class="w-full hover:bg-gray-200 rounded-xl">
-        <div class="flex justify-between items-center">
-            <div class="flex items-center">
-                {#if type == '0'}
-                    <img src={order_icon} alt="" class="h-16 w-16 flex my-4 ml-6" />
-                {:else if type == '1'}
-                    <img src={customer_icon} alt="" class="h-16 w-16 flex my-4 ml-6" />
-                {:else}
-                    <img src={seller_icon} alt="" class="h-16 w-16 flex my-4 ml-6" />
-                {/if}
-                <div class="flex text-left m-4 text-PUA-dark-red">
-                    {firstCol}<br>
-                    Order user : {secondCol}
-                </div>
-            </div>
-            <div class="m-6">
-                {#if type == '0'}
-                    <button class="border-2 border-PUA-dark-red px-7 py-0 text-PUA-dark-red hover:bg-PUA-dark-red hover:text-white">Detail</button>
-                {:else}
-                    {#if (ben)}
-                        <button on:click={changeButtonStatus} class="border-2 border-PUA-dark-red px-7 py-0 text-white bg-PUA-dark-red">Detail</button>
-                    {:else}
-                        <button on:click={changeButtonStatus} class="border-2 border-PUA-dark-red px-7 py-0 text-PUA-dark-red">Detail</button>
-                    {/if}
-                {/if}
-            </div>
-        </div>
-    </button>
+<div
+	bind:this={self}
+	class="mx-12 my-3 flex justify-between rounded-2xl border-2 border-PUA-dark-gray hover:bg-slate-200"
+>
+	<button class="w-full">
+		<div class="flex items-center justify-between">
+			<div class="flex items-center">
+				{#if type == '0'}
+					<img src={order_icon} alt="" class="my-4 ml-6 flex h-16 w-16" />
+				{:else if type == '1'}
+					<img src={customer_icon} alt="" class="my-4 ml-6 flex h-16 w-16" />
+				{:else}
+					<img src={seller_icon} alt="" class="my-4 ml-6 flex h-16 w-16" />
+				{/if}
+				<div class="m-4 flex text-left text-PUA-dark-red">
+					{firstCol}<br />
+					Order user : {secondCol}
+				</div>
+			</div>
+			<div class="m-6">
+				{#if type == '0'}
+					<button
+						class="border-2 border-PUA-dark-red px-7 py-0 text-PUA-dark-red hover:bg-PUA-dark-red hover:text-white"
+						>Detail</button
+					>
+				{:else if ben}
+					<button
+						on:click={changeButtonStatus}
+						class="flex w-24 justify-center border-2 border-PUA-dark-red bg-PUA-dark-red px-7 py-0 text-white"
+						>UnBen</button
+					>
+				{:else}
+					<button
+						on:click={changeButtonStatus}
+						class="w-24 border-2 border-PUA-dark-red px-7 py-0 text-PUA-dark-red">Ben</button
+					>
+				{/if}
+			</div>
+		</div>
+	</button>
 </div>
--- frontend/src/routes/admin/+page.svelte
+++ frontend/src/routes/admin/+page.svelte
@@ -1,108 +1,119 @@
 <script lang="ts">
 	import admin_icon from '$lib/assets/admin_icon.svg';
-    import AdminDataCard from '$lib/components/PUA/adminDataCard.svelte'
+	import AdminDataCard from '$lib/components/PUA/adminDataCard.svelte';
 
-    let orders: {
-        date: string,
-        user: string
-    }[] = [
-        {
-            date: "2020-10-11 19:20:50",
-            user: "user01"
-        },
-        {
-            date: "2020-10-11 19:20:50",
-            user: "user02"
-        },
-        {
-            date: "2020-10-11 19:20:50",
-            user: "user03"
-        },
-        {
-            date: "2020-10-11 19:20:50",
-            user: "user04"
-        }
-    ]
+	let orders: {
+		date: string;
+		user: string;
+	}[] = [
+		{
+			date: '2020-10-11 19:20:50',
+			user: 'user01'
+		},
+		{
+			date: '2020-10-11 19:20:50',
+			user: 'user02'
+		},
+		{
+			date: '2020-10-11 19:20:50',
+			user: 'user03'
+		},
+		{
+			date: '2020-10-11 19:20:50',
+			user: 'user04'
+		}
+	];
 
-    let users: {
-        name: string,
-        email: string,
-        type: string,
-        status: boolean
-    }[] = [
-        {
-            name: "sakana",
-            email: "r1111111",
-            type: '1',
-            status: false
-        },
-        {
-            name: "sakana dewanai",
-            email: "t1111111",
-            type: '2',
-            status: false
-        },
-        {
-            name: "sakana desu",
-            email: "fsddsdss",
-            type: '2',
-            status: true
-        },
-    ]
+	let users: {
+		name: string;
+		email: string;
+		type: string;
+		status: boolean;
+	}[] = [
+		{
+			name: 'sakana',
+			email: 'r1111111',
+			type: '1',
+			status: false
+		},
+		{
+			name: 'sakana dewanai',
+			email: 't1111111',
+			type: '2',
+			status: false
+		},
+		{
+			name: 'sakana desu',
+			email: 'fsddsdss',
+			type: '2',
+			status: true
+		}
+	];
 
-    let userInfo = {
-        user_name:"sakana san",
-        user_email:"t111059001",
-        address:"dsdsadsadasdsadasd",
-        authority:100
-    }
+	let userInfo = {
+		user_name: 'sakana san',
+		user_email: 't111059001',
+		address: 'dsdsadsadasdsadasd',
+		authority: 100
+	};
 
-    
-    function changeDisplay(display_id_name: string, invisible_id_name: string){
-        if (display_id_name == "userData") {
-            document.getElementById("userButton")?.classList.add("bg-white");
-            document.getElementById("userButton")?.classList.add("border-b-4");
-            document.getElementById("orderButton")?.classList.remove("bg-white");
-            document.getElementById("orderButton")?.classList.remove("border-b-4");
-        }
-        else{
-            document.getElementById("orderButton")?.classList.add("bg-white");
-            document.getElementById("orderButton")?.classList.add("border-b-4");
-            document.getElementById("userButton")?.classList.remove("bg-white");
-            document.getElementById("userButton")?.classList.remove("border-b-4");
-        }
+	function changeDisplay(display_id_name: string, invisible_id_name: string) {
+		if (display_id_name == 'userData') {
+			document.getElementById('userButton')?.classList.add('bg-white');
+			document.getElementById('userButton')?.classList.add('border-b-4');
+			document.getElementById('orderButton')?.classList.remove('bg-white');
+			document.getElementById('orderButton')?.classList.remove('border-b-4');
+		} else {
+			document.getElementById('orderButton')?.classList.add('bg-white');
+			document.getElementById('orderButton')?.classList.add('border-b-4');
+			document.getElementById('userButton')?.classList.remove('bg-white');
+			document.getElementById('userButton')?.classList.remove('border-b-4');
+		}
 
-        document.getElementById(display_id_name)?.classList.remove("hidden");
-        document.getElementById(invisible_id_name)?.classList.add("hidden");
-    }
-
+		document.getElementById(display_id_name)?.classList.remove('hidden');
+		document.getElementById(invisible_id_name)?.classList.add('hidden');
+	}
 </script>
 
 <div class="flex justify-center font-bold">
-    <div class="flex w-3/5 flex-col">
-        <div class="flex bg-PUA-stone">
-            <img src={admin_icon} alt="" class="h-28 w-28 flex my-6 ml-10" />
-            <div class="flex flex-col m-7 justify-between">
-                <div>
-                    <div class="text-PUA-gray text-2xl">{userInfo.user_name}</div>
-                    <div class="text-gray-300">{userInfo.user_email}</div>
-                </div>
-                <div class="text-PUA-gray text-xl">{userInfo.address}</div>
-            </div>
-        </div>
-        <div class="flex h-10">
-            <button id="userButton" on:click={ () => changeDisplay("userData", "orderData")} class="w-full bg-gray-200 border-b-4 border-PUA-dark-red bg-white text-PUA-dark-red text-xl">User List</button>
-            <button id="orderButton"on:click={ () => changeDisplay("orderData", "userData")} class="w-full bg-gray-200 border-PUA-dark-red text-PUA-dark-red text-xl">Order List</button>
-        </div>
-        <div id="userData" class="bg-white">
-            {#each users as user}
-                <AdminDataCard firstCol= {user.name} secondCol = {user.email} type = {user.type} ben = {user.status}></AdminDataCard>
-            {/each}
-        </div>
-        <div id="orderData" class="bg-white hidden">
-            {#each orders as order}
-                <AdminDataCard firstCol= {order.date} secondCol = {order.user} type = '0'></AdminDataCard>
-            {/each}
-        </div>
-    </div>
+	<div class="flex w-3/5 flex-col">
+		<div class="flex bg-PUA-stone">
+			<img src={admin_icon} alt="" class="my-6 ml-10 flex h-28 w-28" />
+			<div class="m-7 flex flex-col justify-between">
+				<div>
+					<div class="text-2xl text-PUA-gray">{userInfo.user_name}</div>
+					<div class="text-gray-300">{userInfo.user_email}</div>
+				</div>
+				<div class="text-xl text-PUA-gray">{userInfo.address}</div>
+			</div>
+		</div>
+		<div class="flex h-10">
+			<button
+				id="userButton"
+				on:click={() => changeDisplay('userData', 'orderData')}
+				class="w-full border-b-4 border-PUA-dark-red bg-gray-200 bg-white text-xl text-PUA-dark-red"
+				>User List</button
+			>
+			<button
+				id="orderButton"
+				on:click={() => changeDisplay('orderData', 'userData')}
+				class="w-full border-PUA-dark-red bg-gray-200 text-xl text-PUA-dark-red">Order List</button
+			>
+		</div>
+		<div id="userData" class="bg-white">
+			{#each users as user}
+				<AdminDataCard
+					firstCol={user.name}
+					secondCol={user.email}
+					type={user.type}
+					ben={user.status}
+				></AdminDataCard>
+			{/each}
+		</div>
+		<div id="orderData" class="hidden bg-white">
+			{#each orders as order}
+				<AdminDataCard firstCol={order.date} secondCol={order.user} type="0"></AdminDataCard>
+			{/each}
+		</div>
+	</div>
 </div>

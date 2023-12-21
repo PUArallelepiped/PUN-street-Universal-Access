af575ebb8c42010e150e0db55f26274410d21278
wj461 <wj4wj461@gmail.com>
2023-12-22 00:56:19 +0800

    add more cart order discount data

--- db/i36-event_discount.sql
+++ db/i36-event_discount.sql
@@ -1,4 +1,8 @@
 INSERT INTO event_discount (discount_id, max_quantity, product_id) VALUES
-    (8, 3, 1),
-    (9, 3, 2),
-    (10, 3, 3)
+    (8, 5, 1),
+    (9, 4, 1),
+    (10, 3, 2),
+    (11, 2, 2),
+    (12, 1, 2),
+    (13, 6, 4),
+    (14, 7, 3)

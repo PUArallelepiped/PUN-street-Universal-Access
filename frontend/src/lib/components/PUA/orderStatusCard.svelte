9b0178fef4efea8dffce52894463101531fcb41d
OuChiaYun <j312079@gmail.com>
2023-12-23 19:47:58 +0800

    add animate

--- frontend/src/lib/components/PUA/orderStatusCard.svelte
+++ frontend/src/lib/components/PUA/orderStatusCard.svelte
@@ -1,43 +1,63 @@
 <script lang="ts">
-	import { text } from '@sveltejs/kit';
-
 	export let statusCardContent: {
 		time: string;
 		price: string;
 		src: string;
 		text: string;
 		user: string;
+		status: number;
 	};
+
+	let viewBoxValue = '';
+
+	switch (statusCardContent.status) {
+		case 1:
+			viewBoxValue = '0 0 512 512';
+			break;
+		case 2:
+			viewBoxValue = '0 0 640 512';
+			break;
+		case 3:
+			viewBoxValue = '0 0 640 512';
+			break;
+		case 4:
+			viewBoxValue = '0 0 576 512';
+			break;
+		default:
+			viewBoxValue = '0 0 640 512'; // 默认值，如果 status 不匹配任何情况
+	}
 </script>
 
 <div class=" w-3/7">
-	<div class="flex flex-wrap items-center justify-center gap-8">
-		<button class="flex items-center gap-5 rounded-xl bg-white p-4 shadow-xl">
+	<div class=" flex flex-wrap items-center justify-center gap-8">
+		<button
+			class=" flex items-center gap-5 rounded-xl bg-white p-4 shadow-xl transition-all duration-300 hover:-translate-y-6 hover:transform hover:shadow-2xl hover:shadow-zinc-600"
+		>
 			<svg
 				xmlns="http://www.w3.org/2000/svg"
 				height="80"
 				width="100"
-				viewBox="0 0 640 512"
+				viewBox={viewBoxValue}
 				class="fill-PUA-stone"><path d={statusCardContent.src} /></svg
 			>
 			<div class="w-64 text-start font-bold">
-				<div class=" text-PUA-dark-orange text-xl font-bold leading-relaxed">
+				<div class=" text-xl font-bold leading-relaxed text-PUA-dark-orange">
 					{statusCardContent.time}
 				</div>
 				<div
-					class=" text-PUA-dark-orange flex items-baseline gap-1 text-xl font-bold leading-relaxed"
+					class=" flex items-baseline gap-1 text-xl font-bold leading-relaxed text-PUA-dark-orange"
 				>
 					<p class=" text-base">NT$</p>
 					<p>{statusCardContent.price}</p>
 				</div>
-				<div class=" text-PUA-dark-orange text-xl font-bold leading-relaxed">
+				<div class=" text-xl font-bold leading-relaxed text-PUA-dark-orange">
 					Order user : {statusCardContent.user}
 				</div>
 			</div>
 
 			<button
 				on:click
-				class=" bg-PUA-stone hover:border-PUA-stone hover:text-PUA-stone flex h-20 w-40 items-center justify-center rounded-2xl p-2 text-center font-bold leading-relaxed text-white hover:border-[3px] hover:bg-white"
+				class=" flex h-20 w-40 items-center justify-center rounded-2xl bg-PUA-stone p-2 text-center font-bold leading-relaxed text-white hover:border-[3px] hover:border-PUA-stone hover:bg-white hover:text-PUA-stone"
 			>
 				{statusCardContent.text}
 			</button>
--- frontend/src/routes/order/updateOrderStatus/+page.svelte
+++ frontend/src/routes/order/updateOrderStatus/+page.svelte
@@ -3,8 +3,6 @@
 	import OrderStatusCard from '$lib/components/PUA/orderStatusCard.svelte';
 	import { onMount } from 'svelte';
 
-	// let user_id = 1;
-
 	let Deliver: string =
 		'M48 0C21.5 0 0 21.5 0 48V368c0 26.5 21.5 48 48 48H64c0 53 43 96 96 96s96-43 96-96H384c0 53 43 96 96 96s96-43 96-96h32c17.7 0 32-14.3 32-32s-14.3-32-32-32V288 256 237.3c0-17-6.7-33.3-18.7-45.3L512 114.7c-12-12-28.3-18.7-45.3-18.7H416V48c0-26.5-21.5-48-48-48H48zM416 160h50.7L544 237.3V256H416V160zM112 416a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm368-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z';
 	let Accept: string =
@@ -14,10 +12,10 @@
 	let Making: string =
 		'M320 352c-88.4 0-160-71.6-160-160c0-76.7 62.5-144.7 107.2-179.4c5-3.9 10.9-5.8 16.8-5.8c7.9-.1 16 3.1 22 9.2l46 46 11.3-11.3c11.7-11.7 30.6-12.7 42.3-1C464.5 108 480 160.2 480 192c0 88.4-71.6 160-160 160zm64-111.8c0-36.5-37-73-54.8-88.4c-5.4-4.7-13.1-4.7-18.5 0C293 167.1 256 203.6 256 240.2c0 35.3 28.7 64 64 64s64-28.7 64-64zM32 288c0-17.7 14.3-32 32-32H96c17.7 0 32 14.3 32 32s-14.3 32-32 32v64H544V320c-17.7 0-32-14.3-32-32s14.3-32 32-32h32c17.7 0 32 14.3 32 32v96c17.7 0 32 14.3 32 32v64c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32V416c0-17.7 14.3-32 32-32V288zM320 480a32 32 0 1 0 0-64 32 32 0 1 0 0 64zm160-32a32 32 0 1 0 -64 0 32 32 0 1 0 64 0zM192 480a32 32 0 1 0 0-64 32 32 0 1 0 0 64z';
 	let icon_array = [
-		{ icon: Arrival, text: 'Arrival' },
-		{ icon: Accept, text: 'Accept' },
-		{ icon: Making, text: 'Making' },
-		{ icon: Deliver, text: 'Deliver' }
+		{ icon: Arrival, text: 'Accept' },
+		{ icon: Accept, text: 'Making' },
+		{ icon: Making, text: 'Deliver' },
+		{ icon: Deliver, text: 'Arrival' }
 	];
 
 	type orderRespTye = {
@@ -27,6 +25,7 @@
 		total_price: number;
 		user_id: number;
 		status: number;
+		user_name: string;
 	};
 	let orderRespList: orderRespTye[] = [];
 	let statusCardContent: {
@@ -35,19 +34,14 @@
 		src: string;
 		text: string;
 		user: string;
+		status: number;
 	}[] = [];
-
-	// 	1= 已結帳
-	// 2= 接受訂單
-	// 3= 製作訂單
-	// 4= 運送中
-	// /seller/update-order-status/customer/{userID}/cart/{cartID}/store/{storeID}
 
 	async function postAndChangeStatus(index: number) {
 		const Resp = await fetch(
 			backendPath +
 				`/seller/update-order-status/customer/` +
-				statusCardContent[index].user +
+				orderRespList[index].user_id +
 				`/cart/` +
 				orderRespList[index].cart_id +
 				`/store/` +
@@ -59,19 +53,19 @@
 
 		let orderResp = await Resp.json();
 
-		statusCardContent[index] = {
-			time: orderResp.order_date,
-			price: orderResp.total_price.toString(),
-			src: icon_array[orderResp.status - 1].icon,
-			text: icon_array[orderResp.status - 1].text,
-			user: orderResp.user_id.toString()
-		};
+		if (orderResp.length != 0 && orderResp.status < 5) {
+			statusCardContent[index] = {
+				time: orderResp.order_date,
+				price: orderResp.total_price.toString(),
+				src: icon_array[orderResp.status - 1].icon,
+				text: icon_array[orderResp.status - 1].text,
+				user: orderResp.user_name,
+				status: orderResp.status
+			};
+		} else {
+			statusCardContent = statusCardContent.filter((_, i) => i !== index);
+		}
 
-		// if (res.status == 200) {
-		// 	product_data = await res.json();
-		// }
-		console.log(orderResp);
-		console.log(statusCardContent);
 		return;
 	}
 	async function getOrderRespList() {
@@ -90,12 +84,11 @@
 							price: orderRespList[i].total_price.toString(),
 							src: icon_array[orderRespList[i].status - 1].icon,
 							text: icon_array[orderRespList[i].status - 1].text,
-							user: orderRespList[i].user_id.toString()
+							user: orderRespList[i].user_name,
+							status: orderRespList[i].status
 						}
 					];
 				}
-
-				console.log(statusCardContent);
 			}
 		}
 	}

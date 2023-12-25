b0bd6ec8291a6376903fae23851951d43bbc4c52
nonorze-VOXX <appieosugood@gmail.com>
2023-12-26 03:32:52 +0800

    lint

--- frontend/src/routes/cart/+page.svelte
+++ frontend/src/routes/cart/+page.svelte
@@ -1,25 +1,7 @@
 <script lang="ts">
-	import { backendPath } from '$lib/components/PUA/env';
 	import type { PageData } from './$types';
-	import { goto, invalidateAll } from '$app/navigation';
 	import { Order } from '$lib';
 	export let data: PageData;
-
-	let showDetail = false;
-
-	async function checkout() {
-		//TODO customer id need to change
-		await fetch(backendPath + '/customer/1/checkout', {
-			method: 'POST',
-			body: JSON.stringify({
-				seasoning_discount_id: 1,
-				shipping_discount_id: 1,
-				taking_method: 1
-			})
-		});
-		invalidateAll();
-		return null;
-	}
 </script>
 
 <Order

a88b0666dd168a0d6c1e50bf31b7a8252291b7e1
huhuhu0420 <ghostcheng.00@gmail.com>
2023-12-27 15:57:09 +0800

    updateOrderStatus get id

--- frontend/src/routes/order/updateOrderStatus/+page.server.ts
+++ frontend/src/routes/order/updateOrderStatus/+page.server.ts
@@ -1,10 +1,19 @@
 import { backendPath } from '$lib/components/PUA/env';
+import { getIdByToken } from '$lib/components/PUA/getId';
+import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ cookies }) => {
-	return {
-		orderRespList: await getCart('1', '1')
-	};
+	try{
+		const jwttoken: string = cookies.get('jwttoken') || '';
+		const user_id = await getIdByToken(jwttoken);
+		return {
+			orderRespList: await getCart(user_id)
+		};
+	}
+	catch(e){
+		throw redirect(307, '/login');
+	}
 };
 
 type orderRespTye = {
@@ -13,12 +22,12 @@ type orderRespTye = {
 	order_date: string;
 	total_price: number;
 	user_id: number;
-
 	status: number;
 	user_name: string;
 };
-async function getCart(cartId: string, storeId: string) {
-	const Resp = await fetch(backendPath + '/seller/store/' + '1' + '/orders');
+
+async function getCart(storeId: string) {
+	const Resp = await fetch(backendPath + '/seller/store/' + storeId + '/orders');
 	if (Resp.status == 200) {
 		let orderRespList = await Resp.json();
 

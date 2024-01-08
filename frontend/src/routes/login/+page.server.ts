6cec0e9b60dc095ead92fdbf11b0cee9dd117d76
huhuhu0420 <ghostcheng.00@gmail.com>
2024-01-09 01:04:01 +0800

    very bad set cookie

--- frontend/src/routes/login/+page.server.ts
+++ frontend/src/routes/login/+page.server.ts
@@ -1,8 +1,8 @@
 import { PUBLIC_TWPURL as twpUrl, PUBLIC_BACKEND_PATH as backendPath } from '$env/static/public';
 
 export const actions = {
-    login: async ({ request, cookies }) => {
-        const data = await request.formData();
+	login: async ({ request, cookies }) => {
+		const data = await request.formData();
 		const user_email = data.get('user_email');
 		const password = data.get('password');
 		let maxAge = 60 * 60 * 24; // 1 day
@@ -17,11 +17,11 @@ export const actions = {
 		if (res.status == 200) {
 			await res
 				.json()
-				.then(
-					(data) =>
-					cookies.set('jwttoken', data, { maxAge: maxAge, path: '/' , sameSite: 'strict'})
+				.then((data) =>
+					cookies.set('jwttoken', data, { maxAge: maxAge, sameSite: 'strict', httpOnly: false })
 				);
 			// console.log((await getId()).valueOf());
 		}
 		return res.status;
-}}
+	}
+};

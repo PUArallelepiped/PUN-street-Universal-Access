b53e07a6ae45d6398715e17bd9920d1a1bf9c58c
nonorze-VOXX <appieosugood@gmail.com>
2024-01-22 16:03:09 +0800

    lint

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import '../app.css';
 	import Navbar from '$lib/navbar.svelte';
-	import { page } from '$app/stores';
 	const routes = [
 		{ path: '/profile', title: 'My profile' },
 		{ path: '/shops', title: 'ShopList' },

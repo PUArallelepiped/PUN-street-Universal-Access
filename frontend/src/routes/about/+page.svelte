c2cef9b7d733a0dbf72498883458842e76bc6d7c
OuChiaYun <j312079@gmail.com>
2024-02-04 22:38:12 +0800

    about btw 我不知道要丟哪些內容上去

--- frontend/src/routes/about/+page.svelte
+++ frontend/src/routes/about/+page.svelte
@@ -121,6 +121,28 @@
 						</div>
 					</div>
 				</div>
+				<div class="hover:w-144 peer flex h-40 w-40 duration-500 peer-hover:translate-y-20">
+					<div
+						class="hover:w-144 group peer flex h-40 w-40 cursor-pointer justify-between overflow-hidden rounded-xl bg-white p-4 shadow-2xl duration-500 ease-in-out hover:translate-x-0 hover:translate-y-9 hover:scale-125 hover:shadow-zinc-500"
+					>
+						<img
+							src={watermelon}
+							alt=""
+							width="100"
+							height="100"
+							class="mr-4 h-32 w-32 rounded-full shadow"
+						/>
+						<div
+							class=" group-hover:text-PUA-stone w-96 text-xl font-bold text-transparent transition-all duration-[2000] ease-linear"
+						>
+							<p>+ Name : NOOBDY</p>
+
+							<div class="bg-PUA-stone my-2 h-[2px] w-full"></div>
+							<p>+ Consultant</p>
+							<p>+ DAVE</p>
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>

0eaad8a9ca125f500d94a6cbd27e5d0530441ebc
OuChiaYun <j312079@gmail.com>
2024-02-22 00:13:49 +0800

    add glu,i m sorry ...

--- frontend/src/routes/about/+page.svelte
+++ frontend/src/routes/about/+page.svelte
@@ -135,6 +135,28 @@
 						<div
 							class=" w-96 text-xl font-bold text-transparent transition-all duration-[2000] ease-linear group-hover:text-PUA-stone"
 						>
+							<p>+ Name : GLU</p>
+
+							<div class="my-2 h-[2px] w-full bg-PUA-stone"></div>
+							<p>+ C'est la vie</p>
+							<p>+ worker</p>
+						</div>
+					</div>
+				</div>
+				<div class="peer flex h-40 w-40 duration-500 hover:w-144 peer-hover:translate-y-20">
+					<div
+						class="group peer flex h-40 w-40 cursor-pointer justify-between overflow-hidden rounded-xl bg-white p-4 shadow-2xl duration-500 ease-in-out hover:w-144 hover:translate-x-0 hover:translate-y-9 hover:scale-125 hover:shadow-zinc-500"
+					>
+						<img
+							src={watermelon}
+							alt=""
+							width="100"
+							height="100"
+							class="mr-4 h-32 w-32 rounded-full shadow"
+						/>
+						<div
+							class=" w-96 text-xl font-bold text-transparent transition-all duration-[2000] ease-linear group-hover:text-PUA-stone"
+						>
 							<p>+ Name : NOOBDY</p>
 
 							<div class="my-2 h-[2px] w-full bg-PUA-stone"></div>
